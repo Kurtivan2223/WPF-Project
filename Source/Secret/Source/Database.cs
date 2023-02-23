28813cc4971f410440c8731fe024c8234289b055
Kurtivan2223 <105442868+Kurtivan2223@users.noreply.github.com>
2023-02-23 11:16:20 +0800

    Update Database.cs

--- Source/Secret/Source/Database.cs
+++ Source/Secret/Source/Database.cs
@@ -11,18 +11,6 @@ namespace Secret.Source
 {
     public class Database
     {
-        protected struct Settings
-        {
-            public static string ServerName { get; set; }
-
-            public static uint ServerPort { get; set; }
-
-            public static string UserName { get; set; }
-
-            public static string Password { get; set; }
-
-            public static string Database { get; set; }
-        }
 
         public static string args { get; set; }
 
@@ -32,43 +20,18 @@ namespace Secret.Source
 
         private static MySqlCommand _Command = new MySqlCommand(args, _Connection);
 
-        private static void _GetConnectionString()
-        {
-            if (Directory.Exists(@"Config\\"))
-            {
-                
-                try
-                {
-                    var parser = new FileIniDataParser();
-                    
-                    IniData data = parser.ReadFile("Config\\Data.ini");
-                    
-                    Settings.ServerName = data["DATABASE"]["Server"];
-                    Settings.ServerPort = Convert.ToUInt32(data["DATABASE"]["Port"], 16);
-                    Settings.UserName = data["DATABASE"]["User"];
-                    Settings.Password = data["DATABASE"]["Password"];
-                    Settings.Database = data["DATABASE"]["DBName"];
-                }
-                catch (Exception _Exception)
-                {
-                    MessageBox.Show(_Exception.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-        }
-
         private static void _BuildConnectionString()
         {
-            Secret.Server = Settings.ServerName;
-            Secret.Port = Settings.ServerPort;
-            Secret.UserID = Settings.UserName;
-            Secret.Password = Settings.Password;
-            Secret.Database = Settings.Database;
+            Secret.Server = Configuration.ServerName;
+            Secret.Port = Configuration.ServerPort;
+            Secret.UserID = Configuration.UserName;
+            Secret.Password = Configuration.Password;
+            Secret.Database = Configuration.Database;
             Secret.SslMode = MySqlSslMode.Prefered;
         }
 
         private static void _ConnectDB()
         {
-            _GetConnectionString();
             _BuildConnectionString();
 
             try
